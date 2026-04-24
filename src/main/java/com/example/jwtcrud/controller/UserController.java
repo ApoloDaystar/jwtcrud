42da6e4fd95d556387762502461a9277d0325ca9
Camilo Cantero <camiloandrescantero09@outlook.com>
2026-04-24 18:23:21 -0500

    ApiRest sin jwt, pruebas

---
+++ src/main/java/com/example/jwtcrud/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.example.jwtcrud.controller;
+
+import com.example.jwtcrud.model.User;
+import com.example.jwtcrud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/{username}")
+    public User getUserByUsername(@PathVariable String username) {
+        return userService.findByUsername(username);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping({"/id"})
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
+
+
+    @GetMapping("/test-connection")
+    public List<User> testConnection() {
+        return userService.getAllUsers();
+    }
+}
